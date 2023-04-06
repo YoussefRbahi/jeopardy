58cf59afee64f4d0f467ec5bc52421c99092b7c0
Youssef Rbahi <youssefrbahi@gmail.com>
2023-04-07 00:42:05 +0300

    Looks better. Order of categories not respected.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const container = document.getElementById("container");
-fetch("/categories/1.json")
-  .then((response) => response.json())
-  .then((category) => {
-    const questions = category.questions.map(
-      (q) =>
-        `<div class="question">
+const categories = ["1.json", "2.json", "3.json", "4.json", "5.json", "6.json"];
+function addCat(catFile) {
+  fetch(`/categories/${catFile}`)
+    .then((response) => response.json())
+    .then((category) => {
+      const questions = category.questions.map(
+        (q) =>
+          `<div class="question">
             <p>${q.question}</p>
         </div>`
-    );
-    container.innerHTML +=
-      '<div class="category">' + questions.join("") + "</div>";
+      );
+      container.innerHTML +=
+        '<div class="category">' + questions.join("") + "</div>";
+    });
+}
+function loadBoard() {
+  categories.forEach((file) => {
+    addCat(file);
   });
+}
+loadBoard();
